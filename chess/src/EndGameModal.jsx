78ae9eb3e4a4cd77769ddd501b45fed018ae32e7
danyalutsevich <luchevich31@gmail.com>
2023-03-29 21:50:11 +0300

    alias

---
+++ chess/src/EndGameModal.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Chess } from "chess.js"
+
+export default class EndGameModal extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+    gameEndsWith() {
+
+        let result = ''
+        let by = ''
+
+        if (this.props.isCheckmate) {
+            result = this.props.turn == 'b' ? 'White won' : 'Black won'
+            by = 'by Checkmate'
+        }
+        else if (this.props.isDraw) {
+            result = 'Draw'
+        }
+
+        if (this.props.isStaleMate) {
+            by = 'by Stalemate'
+        }
+        else if (this.props.isInsufficient_material) {
+            by = 'by Insufficient material'
+        }
+        else if (this.props.isIn_threefold_repetition) {
+            by = 'by Threefold repetition'
+        }
+
+
+        return (
+            <div>
+                <h1>{result}</h1>
+                <h2>{by}</h2>
+            </div>
+        )
+
+
+    }
+
+    render() {
+
+        return (
+            <div className="EndGameModalContainer">
+                <div className="EndGameModalContent">
+
+                {this.gameEndsWith()}
+
+                </div>
+            </div>
+        )
+
+    }
+
+
+}
+
---
+++ chess/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, 'src'),
+      "@Board": path.resolve(__dirname, 'src/Board.jsx'),
+      "@EndGameModal": path.resolve(__dirname, 'src/EndGameModal.jsx'),
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', ['@babel/preset-react',{"runtime": "automatic"}]]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  }
+};
